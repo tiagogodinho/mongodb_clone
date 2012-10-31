90dcd4b44ac68eaadc043fdbb1a8f8d93aa1d69c
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-10-31 10:42:42 -0200

    Create a class to dump and restore a mongoDB database.

--- lib/mongodb_clone.rb
+++ lib/mongodb_clone.rb
@@ -3,5 +3,61 @@ require 'mongodb_clone/engine'
 require 'mongodb_clone/version'
 
 module MongodbClone
-  # Your code goes here...
+  class MongodbReplication
+    attr_accessor :id, :path
+
+    def initialize
+      @id = Time.now.utc.strftime('%Y%m%d%H%M%S')
+    end
+
+    def dump(environment = 'production', session = 'default')
+      config = base_config[environment]['sessions'][session]
+
+      params = {
+        h: config['hosts'][0],
+        d: config['database'],
+        u: config['username'],
+        p: config['password'],
+        o: "/tmp/#{ config['database'] }/#{ @id }"
+      }
+
+      a = params.collect { |key, value| "-#{ key } #{ value }" }.join(' ')
+
+      command = "mongodump #{ a }"
+
+      execute(command)
+
+      @path = "#{ params[:o] }/#{ params[:d] }"
+
+      self
+    end
+
+    def restore(environment = 'development', session = 'default')
+      config = base_config[environment]['sessions'][session]
+
+      params = {
+        h: config['hosts'][0],
+        d: config['database'],
+        u: config['username'],
+        p: config['password']
+      }
+
+      a = params.collect { |key, value| "-#{ key } #{ value }" if value }.compact.join(' ')
+
+      command = "mongorestore #{ a } #{ @path }"
+
+      execute(command)
+    end
+
+    private
+
+    def base_config
+      YAML.load_file(Rails.root.join('config/mongoid.yml'))
+    end
+
+    def execute(command)
+      puts command
+      `#{ command }`
+    end
+  end
 end
---
+++ spec/mongodb_clone_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe MongodbClone::MongodbReplication do
+  before do
+    Rails.stub_chain(:root, :join).and_return(File.join(File.dirname(__FILE__), 'support', 'mongoid.yml'))
+  end
+
+  describe '#dump' do
+    it 'should dump database' do
+      subject.id = '201210231843100200'
+      subject.should_receive(:execute).with('mongodump -h mongodb_clone.example.com:27017 -d mongodb_clone_production -u mongodb_clone -p 12345678 -o /tmp/mongodb_clone_production/201210231843100200')
+
+      subject.dump
+    end
+  end
+
+  describe '#restore' do
+    it 'should restore database' do
+      subject.id = '201210231843100200'
+      subject.path = '/tmp/mongodb_clone_production/201210231843100200/mongodb_clone_production'
+      subject.should_receive(:execute).with('mongodump -h mongodb_clone.example.com:27017 -d mongodb_clone_production -u mongodb_clone -p 12345678 -o /tmp/mongodb_clone_production/201210231843100200')
+      subject.should_receive(:execute).with('mongorestore -h localhost:27017 -d mongodb_clone_development /tmp/mongodb_clone_production/201210231843100200/mongodb_clone_production')
+
+      subject.dump.restore
+    end
+  end
+end
